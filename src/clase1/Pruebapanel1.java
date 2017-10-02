37e07abe38190280e1f401ff35918838d225b038
mats94 <matiasbarros5@hotmail.com>
2017-10-02 12:06:08 -0300

    Interfaz grafica V0.1
    implemente la interfaz con algunas personalizaciones a completar, no esta funcional todavia

--- src/clase1/Pruebapanel1.java
+++ src/clase1/Pruebapanel1.java
@@ -1,7 +1,14 @@
 package clase1;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+
 import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -13,10 +20,25 @@ public class Pruebapanel1 extends JPanel{
 	}
 
 		private void initUI() {
-			setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+			// setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+			createLabelTextfield("Nombre :", 20);
+			createLabelTextfield("Apellido :", 20);
+			createLabelTextfield("Profesion :", 20);
+		}
+		private void createLabelTextfield(String campo,int capacidad) {
 			
-			Box nombreBox = Box.createHorizontalBox();
-			nombreBox.add(new JLabel("Nombre: "));
-			nombreBox.add(new JTextField(30));
+			Box box = Box.createVerticalBox();
+			box.add(Box.createVerticalStrut(30));
+			box.add(new JLabel(campo));
+			box.add(Box.createVerticalStrut(10));
+			box.add(wrapTextfield(new JTextField(capacidad)));
+			box.add(Box.createVerticalStrut(10));
+			setBackground(new Color(0, 200, 0));
+			this.add(box);
+		}
+		private Component wrapTextfield(JTextField textField) {
+			JPanel aux = new JPanel();
+			aux.add(textField);
+			return aux;
 		}
 }
--- src/clase1/Test.java
+++ src/clase1/Test.java
@@ -1,13 +1,32 @@
 package clase1;
+import java.awt.Color;
+import java.awt.FlowLayout;
+
+import javax.swing.ImageIcon;
 import javax.swing.JApplet;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JPanel;
 public class Test {
 	public static void main(String[] args) {
-		JFrame frame = new JFrame("texto");
+		JFrame frame = new JFrame("Correo");
+		frame.setTitle("TP CORREO");
+		//menu bar
+		JMenuBar menubar = new JMenuBar();
+		JMenu menu = new JMenu("Menu");
+		JMenu submenu = new JMenu("submenu");
+		menu.add(submenu);
+		menubar.add(menu);
+		
+		//fin menu bar
+		frame.setJMenuBar(menubar);
 		frame.setSize(300, 500);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //para matar tanto grafica como el back(sino te cierra la ventana pero sigue corriendo)
-		
+		//frame.setBackground(new JLabel(new ImageIcon("/Users/matiasbarros/Downloads/correo2.jpg")));
 		frame.getContentPane().add(new Pruebapanel1());
+		
 		
 		frame.setVisible(true);
 	}
