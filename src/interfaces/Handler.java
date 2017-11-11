6eaae675964b2a9347da6f6c9cec694970311bae
mats94 <matiasbarros5@hotmail.com>
2017-11-11 12:51:36 -0300

    menu dinamico
    agregado menu dinamico por funcion "menu()"

--- src/interfaces/Handler.java
+++ src/interfaces/Handler.java
@@ -38,10 +38,27 @@ public class Handler {
 	}
 	public static void principal() {
 		JFrame frame = new JFrame("Correo");
+		frame.setJMenuBar(menu());
 		frame.setTitle("Menu Principal");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
 		frame.getContentPane().add(new Principal());
 		frame.setSize(300, 500);
 		frame.setVisible(true);
+	}
+	private static JMenuBar menu() {
+		JMenuBar menubar = new JMenuBar();
+		JMenu menu = new JMenu("Menu");
+		JMenu submenu = new JMenu("Menu Principal");
+		JMenu submenu2 = new JMenu("Crear Empleado");
+		JMenu submenu3 = new JMenu("Modificar Empleado");
+		JMenu submenu4 = new JMenu("Eliminar Empleado");
+		JMenuItem menuitem = new JMenuItem("menu item 1");
+		submenu.add(menuitem);
+		menu.add(submenu);
+		menu.add(submenu2);
+		menu.add(submenu3);
+		menu.add(submenu4);
+		menubar.add(menu);
+		return menubar;
 	}
 }
